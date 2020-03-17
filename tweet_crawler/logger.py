50cf341e9bf9885e7029e54c51b8d8905f34f953
Li-Xian Chen <twbrandon7@gmail.com>
2020-03-17 22:31:38 +0800

    add log level filter in stream handler

--- tweet_crawler/logger.py
+++ tweet_crawler/logger.py
@@ -6,12 +6,15 @@ LOG_FOLDER = "./logs"
 if not os.path.exists(LOG_FOLDER):
     os.makedirs(LOG_FOLDER)
 
+__stream_hangler = logging.StreamHandler()
+__stream_hangler.setLevel(logging.INFO)
+
 logging.basicConfig(
     level=logging.DEBUG,
-    format='[%(asctime)-15s] %(levelname)s %(message)s',
+    format='[%(asctime)-15s] [%(levelname)s] %(message)s',
     handlers=[
         logging.FileHandler(os.path.join(LOG_FOLDER, 'crawler.log'), 'w', 'utf-8'),
-        logging.StreamHandler(),
+        __stream_hangler,
     ])
 
 debug = logging.debug
